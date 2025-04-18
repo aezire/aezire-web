9599e7127b59cc81c26d698a2b358346620d57e9
aezire <dev@aezire.com>
2025-04-18 15:35:08 -0400

    updated react files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { LoginForm } from './components/login-form'
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <LoginForm/>
+      </div>
+    </>
+  )
+}
+
+export default App
